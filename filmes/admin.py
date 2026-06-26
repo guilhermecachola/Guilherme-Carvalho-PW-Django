4f0f22a06d7325aba46cd207045748c1e639809e
guilhermecachola <a22405584@alunos.ulht.pt>
2026-06-26 22:17:49 +0000

    App filmes: admin configurado

--- filmes/admin.py
+++ filmes/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Filme, Realizador, Genero
 
-# Register your models here.
+@admin.register(Genero)
+class GeneroAdmin(admin.ModelAdmin):
+    list_display = ('nome',)
+    search_fields = ('nome',)
+
+@admin.register(Realizador)
+class RealizadorAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'nacionalidade', 'data_nascimento')
+    search_fields = ('nome', 'nacionalidade')
+
+@admin.register(Filme)
+class FilmeAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'ano', 'realizador', 'classificacao')
+    search_fields = ('titulo',)
+    list_filter = ('ano', 'generos', 'realizador')
+    filter_horizontal = ('generos',)
